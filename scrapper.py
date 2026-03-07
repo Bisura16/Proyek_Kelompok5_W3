16fe06122025f8ee5fc5aff0e8d85a3645c09359
Ghaisank <ghaisan.khoirul.tif425@polban.ac.id>
2026-03-07 16:42:47 +0700

    Tambah logic HTTP request
    add basic HTTP request with headers

--- scrapper.py
+++ scrapper.py
@@ -7,4 +7,11 @@ from bs4 import BeautifulSoup
 
 def scrape_headlines(url):
     """Fungsi utama untuk scraping headline berita."""
-    pass
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+    }
+
+    response = requests.get(url, headers=headers, timeout=10)
+    html = response.text
+
+    return html
