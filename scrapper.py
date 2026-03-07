fc37b940ba3ed01d65895686d082e64c3d8dd4b6
Ghaisank <ghaisan.khoirul.tif425@polban.ac.id>
2026-03-07 16:43:27 +0700

    Parsing headline pakai BeautifulSoup
    add BeautifulSoup parsing for headlines

--- scrapper.py
+++ scrapper.py
@@ -14,4 +14,18 @@ def scrape_headlines(url):
     response = requests.get(url, headers=headers, timeout=10)
     html = response.text
 
-    return html
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Parsing untuk detik.com
+    articles = soup.find_all("article")
+    headlines = []
+
+    for article in articles:
+        link_tag = article.find("a")
+        if link_tag:
+            judul = link_tag.get_text(strip=True)
+            link = link_tag.get("href", "")
+            if judul:
+                headlines.append({"judul": judul, "link": link})
+
+    return headlines
